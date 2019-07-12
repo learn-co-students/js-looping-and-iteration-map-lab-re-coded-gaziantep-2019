802596cc619e04d22ff09e992320fca303b11a1e
Abdelnasser Alkhellow <abdelnasser.kh10@gmail.com>
2019-07-12 07:29:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file.
+
+
+// function lowerCaseDrivers(drivers){
+//   return drivers.map(function(driver){
+//     return driver.toLowerCase();
+//   });
+// }
+
+function lowerCaseDrivers(arr){
+  return arr.map(el =>  el.toLowerCase() );
+}
+function nameToAttributes(arr){
+  return arr.map(
+    el => Object.assign( {firstName: el.split(" ")[0]},{lastName: el.split(" ")[1]}));
+}
+function attributesToPhrase(arr){
+  return arr.map(el => `${el.name} is from ${el.hometown}` );
+}
