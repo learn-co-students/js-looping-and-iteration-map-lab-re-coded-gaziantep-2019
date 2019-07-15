40b95cc0e93ac4a0001086175b1bb17c6e2a903c
Eren Dogan <eren.dogan@gaziulas.com.tr>
2019-07-15 07:59:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file.
+function lowerCaseDrivers(array){
+  
+  const newdrivers=array.map(function(num){
+    return num.toLowerCase();
+  });
+  return newdrivers;
+}
+
+function nameToAttributes(array){
+  const newdrivers=array.map(function(num){
+    const first=num.split(" ")[0];
+    const last=num.split(" ")[1];
+    return {firstName: first, lastName: last};
+  });
+  return newdrivers;
+}
+
+function attributesToPhrase(array){
+  const newdrivers=array.map(function(num){
+    const element=num.name;
+    const element2=num.hometown;
+    return element+' is from '+element2;
+  });
+  return newdrivers;
+}
+
+
+
+
+
+ const drivers = [
+        { name: 'Bobby', hometown: 'Pittsburgh' },
+        { name: 'Sammy', hometown: 'New York' },
+        { name: 'Sally', hometown: 'Cleveland' },
+        { name: 'Annette', hometown: 'Los Angeles' },
+        { name: 'Bobby', hometown: 'Tampa Bay' }
+      ];
